3154c1cd5b434278a6b53546ddd182ff0d22a04c
theh4ru <theh4ru@gmail.com>
2025-05-21 10:41:54 +0900

    test

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactStrictMode: true,
+  output: 'export',
+  images: {
+    loader: 'custom',
+    loaderFile: './src/utils/my-loader.ts',
+  },
+
 };
 
 export default nextConfig;
