858beeb6460c583b1f44441e52d84cbf65377264
theh4ru <theh4ru@gmail.com>
2025-05-21 11:38:10 +0900

    Move static site to docs/

--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,12 @@ const repo = 'test';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  images: {
-    loader: 'custom',
-    loaderFile: './src/utils/my-loader.ts',
-  },
-  basePath: `/${repo}`,
-  assetPrefix: `/${repo}/`,
+  // images: {
+  //   loader: 'custom',
+  //   loaderFile: './src/utils/my-loader.ts',
+  // },
+  // basePath: `/${repo}`,
+  // assetPrefix: `/${repo}/`,
 };
 
 export default nextConfig;
